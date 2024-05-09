e6af65773d459a5a61dfe68f5877d7e2c364b4c0
Ankush90412 <163531116+Ankush90412@users.noreply.github.com>
2024-05-09 13:26:08 +0530

    Update YourPHPFile.php

--- src/YourPHPFile.php
+++ src/YourPHPFile.php
@@ -1,11 +1,11 @@
 <?php
 
-class Calculator 
+class Calculator {
     public function add($a, $b) {
         return $a + $b;
     }
 
     public function subtract($a, $b) {
-        return $a - $b
+        return $a - $b;
     }
 }
